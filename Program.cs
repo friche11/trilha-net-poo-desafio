37049f7371811d48caed901f54f74250f12579a1
friche11 <vicfriche@gmail.com>
2024-05-24 16:48:55 -0300

    feat: execução do programa no console

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("123456", "modelo 1", "111111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("456123", "modelo 2", "222222222", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
